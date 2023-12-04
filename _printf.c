5bb1616c7443d56f6a6fbdc5aa514478c8c92567
Jecolia Tieleo <jecoliatieleo@gmail.com>
2023-12-04 00:45:40 -0800

    .

--- _printf.c
+++ _printf.c
@@ -7,7 +7,23 @@
  */
 int _printf(const char *format, ...)
 {
+	int length = 0;
 	va_list arguments;
 	va_start(arguments, format);
 
-}
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+		}
+		else
+		{
+			format++;
+		}
+
