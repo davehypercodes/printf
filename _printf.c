8bc3c6d0a56335c31a281033565ae0363f31b22f
Jecolia Tieleo <jecoliatieleo@gmail.com>
2023-12-04 01:28:10 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
+ * _printf - produces output according to a format
+ * @format: the concerning format
  *
- *
- *
+ * Return: the number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
 	int length = 0;
+	int i;
 	va_list arguments;
 	va_start(arguments, format);
 
@@ -21,18 +24,27 @@ int _printf(const char *format, ...)
 		if (*format != '%')
 		{
 			write(1, format, 1);
+			length++;
 		}
 		else
 		{
 			format++;
 		}
+
+		if (format == '\0')
+		{
+			break;
+		}
+
 		switch (format)
 		{
 			case 'c':{
-
+					 i = va_arg(arguments, int);
 				 }
+				 write(1, i, strlen(i));
 			case 's':{
 
+					 strlen(
 				 }
 			case '%':{
 
