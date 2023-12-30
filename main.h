029f04a76fc115688c2ef979c44dc8265323bfa1
DAVE <jesugbohunmi@outlook.com>
2023-12-30 16:39:14 +0100

    Add int handling and number printing functions

--- main.h
+++ main.h
@@ -8,5 +8,7 @@ int _printf(const char *format, ...);
 int handle_char_str(char specifier, va_list args);
 int process_string(char *s);
 int _detect_format(char format, va_list args);
+int handle_int(char specifier, va_list args);
+int print_number(int n);
 
 #endif
